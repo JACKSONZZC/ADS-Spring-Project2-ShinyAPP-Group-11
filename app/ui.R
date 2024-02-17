a9bc6c8e6adb4e987a6d36faa4983176111f921d
Jackson Zhao <zz3119@columbia.edu>
2024-02-17 11:37:38 -0500

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -11,27 +11,7 @@ if (!require("shinythemes")) {
   install.packages("shinythemes")
   library(shinythemes)
 }
-if (!require("leaflet")) {
-  install.packages("leaflet")
-  library(leaflet)
-}
-if (!require("leaflet.extras")) {
-  install.packages("leaflet.extras")
-  library(leaflet.extras)
-}
-
-# Define UI for application that draws a histogram
-shinyUI(
-    navbarPage(strong("Citi Bike Study",style="color: white;"), 
-               theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         icon = icon("map-marker-alt"), #choose the icon for
-                         div(class = 'outer',
-                        # side by side plots
-                        fluidRow(
-                                splitLayout(cellWidths = c("50%", "50%"), 
-                                             leafletOutput("left_map",width="100%",height=1200),
+    leafletOutput("left_map",width="100%",height=1200),
                                              leafletOutput("right_map",width="100%",height=1200))),
                         #control panel on the left
                         absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
